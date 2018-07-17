b8e2d24446d84eb535ca4f9146ebe7592187403c
github username <github email address>
2018-07-17 15:04:52 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,20 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  onInputChange = (e) => {
+    let name = e.target.name
+    let tarVal = e.target.value
+
+    this.setState({
+      name: e.target.value
+    });
+
   }
 
   render() {
@@ -13,13 +26,21 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input
+              id="test-username"
+              type="text"
+              name="username"
+              onChange={this.onInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input
+              id="test-password"
+              type="password"
+              name="password"
+              onChange={this.onInputChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = { message: "" };
+  }
+
+  updatedMessage = (e) => {
+    return this.setState({message: e.target.value});
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.maxChars}
+          onChange={this.updatedMessage} />
+        Characters remaining: ({this.props.maxChars - this.state.message.length})
       </div>
     );
   }
